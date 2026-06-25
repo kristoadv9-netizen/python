76c99ff245836af6f40ad975d8b172a5209be7a4
kristoadv9-netizen <kristoadv9@gmail.com>
2026-06-25 12:21:00 -0400

    Update output message to use Celsius symbol

--- primeiro teste.py
+++ primeiro teste.py
@@ -2,4 +2,4 @@ fahrenheit = float(input("Digite a temperatura em Fahrenheit: "))
 
 celsius = (fahrenheit - 32) * 5 / 9
 
-print(f"O valor convertido é: {celsius: .0f} picas ")
+print(f"O valor convertido é: {celsius: .0f} °C ")
